e4cd880699d08c82ccceade1492565a263ebe393
vvonderfull <Igor27igor10igor99>
2020-04-13 19:08:30 +0700

    #igor 13.04.20

---
+++ src/components/Map.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="map-background">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Map.vue"
+    }
+</script>
+
+<style scoped>
+    .map-background {
+        width: 100%;
+        height: 100%;
+        background: url("/src/assets/map.svg");
+    }
+</style>
