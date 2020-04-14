9ae352f335bb1e807caf12b87c85fac83928f2f4
Alisa <kozhemyakina_alice@smartworld.team>
2020-04-14 11:40:59 +0700

    all dots

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,350 +1,380 @@
 <template>
-    <div class="map-background">
-        <img src="../assets/map.svg"/>
-        <div class="tomsk"></div>
-        <div class="burgas"></div>
-        <div class="varna"></div>
-        <div class="belgorod"></div>
-        <div class="keninsberg"></div>
-        <div class="kaunas"></div>
-        <div class="vitebsk"></div>
-        <div class="riga"></div>
-        <div class="orsha"></div>
-        <div class="smolensk"></div>
-        <div class="elnya"></div>
-        <div class="asino"></div>
-        <div class="novosibirsk"></div>
-        <div class="yurga"></div>
-        <div class="vologda"></div>
-        <div class="moskva"></div>
-        <div class="vyazma"></div>
-        <div class="white"></div>
-        <div class="duhovshina"></div>
-        <div class="kastornoe"></div>
-        <div class="stalingrad"></div>
-        <div class="zaporoje"></div>
-        <div class="odessa"></div>
-        <div class="odessa-korovel"></div>
-        <div class="korovel"></div>
-        <div class="lubvin"></div>
-        <div class="lodz"></div>
-        <div class="berlin"></div>
-        <div class="st-russa"></div>
-        <div class="kr-rog"></div>
-        <div class="poznan"></div>
-        <div class="magdeburg"></div>
-    </div>
+  <div class="map-background">
+    <img src="../assets/map.svg" />
+    <div class="tomsk"></div>
+    <div class="burgas"></div>
+    <div class="varna"></div>
+    <div class="belgorod"></div>
+    <div class="keninsberg"></div>
+    <div class="kaunas"></div>
+    <div class="vitebsk"></div>
+    <div class="riga"></div>
+    <div class="orsha"></div>
+    <div class="smolensk"></div>
+    <div class="elnya"></div>
+    <div class="asino"></div>
+    <div class="novosibirsk"></div>
+    <div class="yurga"></div>
+    <div class="vologda"></div>
+    <div class="moskva"></div>
+    <div class="vyazma"></div>
+    <div class="white"></div>
+    <div class="duhovshina"></div>
+    <div class="kastornoe"></div>
+    <div class="stalingrad"></div>
+    <div class="zaporoje"></div>
+    <div class="odessa"></div>
+    <div class="odessa-korovel"></div>
+    <div class="korovel"></div>
+    <div class="lubvin"></div>
+    <div class="lodz"></div>
+    <div class="berlin"></div>
+    <div class="st-russa"></div>
+    <div class="kr-rog"></div>
+    <div class="poznan"></div>
+    <div class="magdeburg"></div>
+    <div class="sinyavino"></div>
+    <div class="tihvin"></div>
+    <div class="myasnoy-bor"></div>
+    <div class="vel-luki"></div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Map.vue",
-        mounted() {
-            console.log(window);
-        }
-    };
+export default {
+  name: "Map.vue",
+  mounted() {
+    console.log(window);
+  }
+};
 </script>
 
 <style scoped>
-    img {
-        width: 100%;
-        height: 100%;
-    }
+img {
+  width: 100%;
+  height: 100%;
+}
 
-    .map-background {
-        width: 100%;
-        height: 100%;
-    }
+.map-background {
+  width: 100%;
+  height: 100%;
+}
 
-    .tomsk {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 47.3%;
-        left: 79.6%;
-    }
+.tomsk {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 47.3%;
+  left: 79.6%;
+}
 
-    .burgas {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        left: 24.6%;
-        bottom: 6%;
-    }
+.burgas {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  left: 24.6%;
+  bottom: 6%;
+}
 
-    .varna {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        left: 25.8%;
-        bottom: 12.2%;
-    }
+.varna {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  left: 25.8%;
+  bottom: 12.2%;
+}
 
-    .belgorod {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        left: 41.7%;
-        bottom: 38.7%;
-    }
+.belgorod {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  left: 41.7%;
+  bottom: 38.7%;
+}
 
-    .keninsberg {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        left: 23.2%;
-        bottom: 52.1%;
+.keninsberg {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  left: 23.2%;
+  bottom: 52.1%;
+}
 
-    }
+.kaunas {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  left: 26.2%;
+  bottom: 53.7%;
+}
 
-    .kaunas {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        left: 26.2%;
-        bottom: 53.7%;
+.vitebsk {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  left: 33.5%;
+  bottom: 54.6%;
+}
 
-    }
+.riga {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  left: 29.9%;
+  bottom: 61.9%;
+}
 
-    .vitebsk {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        left: 33.5%;
-        bottom: 54.6%;
-    }
+.orsha {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  left: 33.2%;
+  bottom: 53.7%;
+}
 
-    .riga {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        left: 29.9%;
-        bottom: 61.9%;
-    }
+.smolensk {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  left: 35.4%;
+  bottom: 53.3%;
+}
 
-    .orsha {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        left: 33.2%;
-        bottom: 53.7%;
-    }
+.elnya {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  left: 37.9%;
+  bottom: 53%;
+}
 
-    .smolensk {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        left: 35.4%;
-        bottom: 53.3%;
-    }
+.asino {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 65.5%;
+  left: 82.6%;
+}
 
-    .elnya {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        left: 37.9%;
-        bottom: 53%;
-    }
+.novosibirsk {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 53%;
+  left: 75.8%;
+}
 
-    .asino {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 65.5%;
-        left: 82.6%;
-    }
+.yurga {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 53.9%;
+  left: 84%;
+}
 
-    .novosibirsk {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 53.0%;
-        left: 75.8%;
-    }
+.vologda {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 75.4%;
+  left: 50.9%;
+}
 
-    .yurga {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 53.9%;
-        left: 84%;
-    }
+.moskva {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 64.4%;
+  left: 45.9%;
+}
 
-    .vologda {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 75.4%;
-        left: 50.9%;
-    }
+.vyazma {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 61.5%;
+  left: 41.8%;
+}
 
-    .moskva {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 64.4%;
-        left: 45.9%;
-    }
+.white {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 65.8%;
+  left: 37.1%;
+}
 
-    .vyazma {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 61.5%;
-        left: 41.8%;
-    }
+.duhovshina {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 64.4%;
+  left: 37.5%;
+}
 
-    .white {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 65.8%;
-        left: 37.1%;
+.kastornoe {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 58.7%;
+  left: 47.7%;
+}
 
-    }
+.stalingrad {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 48.7%;
+  left: 54.1%;
+}
 
-    .duhovshina {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 64.4%;
-        left: 37.5%;
+.zaporoje {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 41.8%;
+  left: 40.6%;
+}
 
-    }
+.odessa {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 41.7%;
+  left: 32.6%;
+}
 
-    .kastornoe {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 58.7%;
-        left: 47.7%;
-    }
+.odessa-korovel {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 42.9%;
+  left: 28.2%;
+}
 
-    .stalingrad {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 48.7%;
-        left: 54.1%;
-    }
+.korovel {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 53.8%;
+  left: 26.1%;
+}
 
-    .zaporoje {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 41.8%;
-        left: 40.6%;
-    }
+.lubvin {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 56.1%;
+  left: 22.3%;
+}
 
-    .odessa {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 41.7%;
-        left: 32.6%;
-    }
+.lodz {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 61.5%;
+  left: 20.2%;
+}
 
-    .odessa-korovel {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 42.9%;
-        left: 28.2%;
-    }
+.berlin {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 72%;
+  left: 15.6%;
+}
 
-    .korovel {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 53.8%;
-        left: 26.1%;
-    }
+.st-russa {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 78%;
+  left: 38.6%;
+}
 
-    .lubvin {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 56.1%;
-        left: 22.3%;
-    }
-
-    .lodz {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 61.5%;
-        left: 20.2%;
-
-    }
-
-    .berlin {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 72.0%;
-        left: 15.6%;
-    }
-
-    .st-russa {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 78%;
-        left: 38.6%;
-    }
-
-    .kr-rog {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 51.1%;
-        left: 37.9%;
-
-    }
-    .poznan {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 70%;
-        left: 19.8%;
-    }
-    .magdeburg {
-        background-color: blue;
-        width: 10px;
-        height: 10px;
-        position: relative;
-        bottom: 78.5%;
-        left: 13.8%;
-    }
+.kr-rog {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 51.1%;
+  left: 37.9%;
+}
+.poznan {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 70%;
+  left: 19.8%;
+}
+.magdeburg {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 78.5%;
+  left: 13.8%;
+}
+.sinyavino {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 92.8%;
+  left: 37.8%;
+}
+.tihvin {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 90.8%;
+  left: 39.8%;
+}
+.myasnoy-bor {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 90.5%;
+  left: 38.3%;
+}
+.vel-luki {
+  background-color: blue;
+  width: 10px;
+  height: 10px;
+  position: relative;
+  bottom: 85.6%;
+  left: 35.4%;
+}
 </style>
